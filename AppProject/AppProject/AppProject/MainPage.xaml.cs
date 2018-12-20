eab545e5bbbe514e02994cbc79a16b83f18e1fc5
ElijahKeane <45961692+ElijahKeane@users.noreply.github.com>
2018-12-20 11:55:32 +0000

    Basic design of main page set up with button that produce alert messages as a placeholder for the other pages

---
+++ AppProject/AppProject/AppProject/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (Device.OS == TargetPlatform.iOS)
+                Padding = new Thickness(0, 20, 0, 0);
+        }
+
+        private void Torch_Clicked(object sender, EventArgs e) => DisplayAlert("Title", "Torch Page", "Ok");
+        private void Calculator_Clicked(object sender, EventArgs e) => DisplayAlert("Title", "Calcuator Page", "Ok");
+        private void BMICalculator_Clicked(object sender, EventArgs e) => DisplayAlert("Title", "BMI Calculator Page", "Ok");
+    }
+}
